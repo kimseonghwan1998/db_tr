bc403895a99bc0d897238d90c3d5527b83ee8b66
ksh <fjscu112@naver.com>
2022-08-08 14:45:05 +0900

    1111

--- mysql/sql/Runday.sql
+++ mysql/sql/Runday.sql
@@ -1,8 +1,4 @@
-
--- 로그인
-select * from member where id = "fjscu112" and pw="1234"
-;
--- 목록들을 쭉 써내려가
+use love;
 
 -- 코드 그룹
 select
@@ -14,26 +10,59 @@ select
 from RunCodeGroup a
 inner join RunCode b on b.RunCodeGroup_seq = a.seq;
 
+-- 로그인
+select id,pw
+ from member 
+;
+
+-- 주행 거리 
+select
+	c.run_km,
+	c.time,
+    c.pace,
+    c.kcal,
+    d.name,
+    d.gender,
+    d.district
+from run_distance c
+left join member d on d.seq = c.member_member;
+
+
 -- 러닝 크루 모집
 select
-	c.seq,
-	c.city,
-    c.district,
-    c.PostalCode,
-    d.recruit
-from member c
-left join run_crew d on d.member_member = c.seq;
+	y.seq,
+    y.region,
+    y.time,
+	y.recruit,
+    x.name,
+	x.city,
+    x.district,
+    x.PostalCode  
+from run_crew y
+left join member x on x.seq = y.member_member;
+
+-- 러닝크루 지원상세 
 
 -- 게시판
 select
-	e.seq,
-    e.name,
-    f.num,
-    f.title,
-    f.date,
-    f.hits,
-    f.writer
-from member e
-left join board f on f.writer = e.seq;
+    e.num,
+    e.title,
+    e.date,
+    e.hits,
+    e.writer,
+	f.seq,
+    f.name
+from board e
+left join member f on f.seq = e.writer;
 
+-- 러닝 크루 
+select
+	g.name,
+    g.city,
+    g.district,
+    h.crew_name,
+    h.time,
+    h.recruit
+from member g
+left join run_crew h on h.member_member = g.seq;
 
